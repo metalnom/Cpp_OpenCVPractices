69c6244ea37f0273c47888b1db595d5c83285726
Changdo.Jung <53599500+metalnom@users.noreply.github.com>
2019-11-18 19:03:29 +0900

    Create OpenCVpr01.cpp

---
+++ OpenCVpr01.cpp
@@ -0,0 +1,34 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(void) {
+	cout << "Hello OpenCV" << CV_VERSION << endl;
+	int x, y;
+	Mat img;
+	img = imread("lenna.tif", IMREAD_GRAYSCALE);
+
+	if(img.empty()) {
+		cerr << "Image load failed!" << endl;
+		return -1;
+	}
+
+	namedWindow("image");
+	imshow("image", img);
+
+	for(x=100, y=100;x<400;x++, y++) {
+		moveWindow("image", x, y);
+		waitKey(10);
+	}
+	for(;x<800;x++, y--) {
+		moveWindow("image", x, y);
+		waitKey(10);
+	}
+
+	waitKey(50);
+	destroyAllWindows();
+
+	return 0;
+}
