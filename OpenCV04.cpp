f02a503f00af63082952a00eebab831952bedf47
Changdo.Jung <53599500+metalnom@users.noreply.github.com>
2019-11-18 20:50:16 +0900

    Create OpenCV04.cpp

---
+++ OpenCV04.cpp
@@ -0,0 +1,22 @@
+// Mat 객체 생성
+// 초기값 설정
+
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main(void) {
+
+	Mat img1(480, 640, CV_8UC1, Scalar(128));
+	Mat img2(480, 640, CV_8UC3, Scalar(100, 100, 150));
+
+	imshow("img1", img1);
+	imshow("img2", img2);
+
+	waitKey();
+
+	return 0;
+}
