82b45849fe96c9c5f998a9ede72597ad2886cabb
Changdo.Jung <53599500+metalnom@users.noreply.github.com>
2019-11-18 20:57:50 +0900

    Update OpenCV04.cpp

--- OpenCV04.cpp
+++ OpenCV04.cpp
@@ -13,8 +13,12 @@ int main(void) {
 	Mat img1(480, 640, CV_8UC1, Scalar(128));
 	Mat img2(480, 640, CV_8UC3, Scalar(100, 100, 150));
 
+	Mat img3;
+	img3 = Mat::eye(20, 20, CV_8UC1) * 255;
+
 	imshow("img1", img1);
 	imshow("img2", img2);
+	imshow("img3", img3);
 
 	waitKey();
 
